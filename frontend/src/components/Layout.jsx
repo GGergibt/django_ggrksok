7cbaead4adfda7d62371b42d14de55cdd1539e02
GGerbit <gosha2007naz@gmail.com>
2022-07-14 22:20:32 +0300

    add colors in nav Layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -19,27 +19,28 @@ const Layout = ({cookies}) => {
 
 	return (
 		<>
-		  <header>
+		  <header className="">
 
 
+
+			  <div className="bg-gray-500 flex">
 			  {/* <a href="/api"> home</a> */}
 			  {/* <a href="/api/posts"> blog</a> */}
-	          <div className="inline-flex rounded-md shadow-sm my-1" role="group">
-			  {<Link to="/" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+			  {<Link to="/" className="py-2 px-4 text-sm font-medium text-gray-900 rounded-l-lg hover:bg-gray-600 hover:text-blue-700 focus:z-10  focus:ring-blue-700 focus:text-red-700 dark:bg-gray-700 bg-gray-500 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
 			  Home
 		  </Link>}
-			  {<Link to="/registration" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+			  {<Link to="/registration" className="py-2 px-4 text-sm font-medium text-gray-900  hover:bg-gray-600 hover:text-blue-700 focus:z-10 focus:ring-blue-700 bg-gray-500 focus:text-red-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
 	           Зарегистрироваться 
 		  </Link>}
-			  {<Link to="/login" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+			  {<Link to="/login" className="py-2 px-4 text-sm font-medium text-gray-900  rounded-r-md hover:bg-gray-600 hover:text-blue-700 focus:z-10   bg-gray-500 focus:text-red-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
 			  Войти
 		  </Link>}
-			  {cookies.token? <Link to="/history" className="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+			  {cookies.token? <Link to="/history" className="py-2 px-4 text-sm font-medium text-gray-900 rounded-r-md hover:bg-gray-600 hover:text-blue-700  focus:ring-blue-700 bg-gray-500 focus:text-red-600 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
 				  История скачиваний
 			  </Link> : <h1></h1>}
-</div>
+			  </div>
 		  </header> 
-			  <div className="container my-10 mx-auto w-1/2">
+			  <div className="container my-10 mx-auto w-2/5">
 			<Outlet/>
 			  </div>
 			  </>
